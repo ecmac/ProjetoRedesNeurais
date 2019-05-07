44e528f0b36df53b6a0000bf4a0fc7d57d6c7cef
Sérgio Filho <sergiovf_@hotmail.com>
2019-05-06 21:37:53 -0300

    exemplo de como usar a base de dados

---
+++ recuperar base de dados.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  6 21:24:51 2019
+
+@author: sergi
+"""
+
+import pickle
+import gzip
+
+caminho = './base de dados'
+
+# Só mudar o número 0 para outro, de 1 a 4, para acessar as outras bases
+with gzip.open(caminho + ' 0', 'rb') as arquivo:
+    treino, validacao, teste = pickle.load(arquivo)
